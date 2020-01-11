b0096ca18976e1d6b155b9243723f229ff433631
qinghe <iwangqinghe@gmail.com>
2020-01-11 12:32:46 +0800

    binarySearch

--- 02/Q15.js
+++ 02/Q15.js
@@ -1,29 +1,24 @@
 
-const log = console.log;
 
-const insertSort = (A, j = A.length - 1, ) => {
+const binarySearch = (A, target, p = 0, r = A.length -  1) => {
+    const q = Math.floor((p + r) / 2)
+    // console.log(p, q, r)
 
-    if (j === 1) {
-        return
+    if (A[q] === target) {
+        return q
+    } else if (A[q] < target) {
+        return binarySearch(A, target, q+1, r)
+    } else {
+        return binarySearch(A, target, p, q)
     }
-    
-    insertSort(A, j - 1)
-
-    const key = A[j]
-    let i;
-    for(i = j - 1; i > -1; i--) {
-        if (A[i] > key) {
-            A[i + 1] = A[i]
-        } else {
-            break;
-        }
-    }
-    A[i + 1] = key;
 }
 
-
 if(require.main === module) {
-    const list = [38, 545, 6, 9, 324, 1, 4, 564,17, 754]
-    insertSort(list)
-    log(list)
+    const list = [0, 1, 2, 3, 4, 5, 6]
+    console.log(binarySearch(list, 0))
+    console.log(binarySearch(list, 1))
+    console.log(binarySearch(list, 2))
+    console.log(binarySearch(list, 3))
+    console.log(binarySearch(list, 4))
+    console.log(binarySearch(list, 5))
 }
