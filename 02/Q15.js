052b6bd78d92d5b3e8ee0d84fcb7fd21a356f7a8
qinghe <iwangqinghe@gmail.com>
2020-01-11 12:15:28 +0800

    recursice insert

---
+++ 02/Q15.js
@@ -0,0 +1,29 @@
+
+const log = console.log;
+
+const insertSort = (A, j = A.length - 1, ) => {
+
+    if (j === 1) {
+        return
+    }
+    
+    insertSort(A, j - 1)
+
+    const key = A[j]
+    let i;
+    for(i = j - 1; i > -1; i--) {
+        if (A[i] > key) {
+            A[i + 1] = A[i]
+        } else {
+            break;
+        }
+    }
+    A[i + 1] = key;
+}
+
+
+if(require.main === module) {
+    const list = [38, 545, 6, 9, 324, 1, 4, 564,17, 754]
+    insertSort(list)
+    log(list)
+}
